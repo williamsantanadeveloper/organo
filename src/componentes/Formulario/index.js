866c4915ce96b32a82271ac6c979f0e88f3c33be
William Santana <williamandrade1058@gmail.com>
2024-05-06 15:14:04 -0300

    up

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -4,16 +4,18 @@ import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
 import './formulario.css'
 
-const Formulario = ({aoCadastrar, times}) => {
+const Formulario = ({ aoCadastrar, times, cadastrarTime }) => {
 
     const [nome, setNome] = useState('')
     const [cargo, setCargo] = useState('')
     const [imagem, setImagem] = useState('')
     const [time, setTime] = useState('')
+    const [nomeTime, setNomeTime] = useState('')
+    const [corTime, setCorTime] = useState('')
 
     const aoSubmeter = (evento) => {
         evento.preventDefault()
-        console.log('form enviado', nome, cargo, imagem, time )
+        console.log('form enviado', nome, cargo, imagem, time)
         aoCadastrar({
             nome,
             cargo,
@@ -31,24 +33,45 @@ const Formulario = ({aoCadastrar, times}) => {
                     label='Nome'
                     placeholder='Digite seu nome '
                     valor={nome}
-                    aoAlterado={valor => setNome(valor)}/>
+                    aoAlterado={valor => setNome(valor)} />
                 <CampoTexto
                     obrigatorio={true}
-                    label='Cargo' 
+                    label='Cargo'
                     placeholder='Digite seu cargo '
                     valor={cargo}
-                    aoAlterado={valor => setCargo(valor)}/>
-                <CampoTexto 
-                    label='Imagem' 
+                    aoAlterado={valor => setCargo(valor)} />
+                <CampoTexto
+                    label='Imagem'
                     placeholder='Informe o endereço da imagem '
-                    aoAlterado={valor => setImagem(valor)}/>
-                <ListaSuspensa 
+                    aoAlterado={valor => setImagem(valor)} />
+                <ListaSuspensa
                     obrigatorio={true}
                     label='Times'
-                    items={times} 
+                    items={times}
                     valor={time}
-                    aoAlterado={valor => setTime(valor)}/>
+                    aoAlterado={valor => setTime(valor)} />
                 <Botao texto='Criar card' />
+            </form>
+            <form className="formulario" onSubmit={(evento) => {
+                evento.preventDefault();
+                cadastrarTime({ nome: nomeTime, cor: corTime })
+            }}>
+                <h2>Preencha os dados para criar um novo time.</h2>
+                <CampoTexto
+                    obrigatorio
+                    label='Nome'
+                    placeholder='Digite o nome do time'
+                    valor={nomeTime}
+                    aoAlterado={valor => setNomeTime(valor)}
+                />
+                <CampoTexto
+                    obrigatorio={true}
+                    label='Cor'
+                    placeholder='Digite a cor do time'
+                    valor={corTime}
+                    aoAlterado={valor => setCorTime(valor)}
+                />
+                <Botao texto='Criar um novo time' />
             </form>
         </section>
     )
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -5,7 +5,7 @@ import './time.css'
 const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
     return (
         colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: hexToRgba(time.cor, 0.6) }}>
-            <input onChange={evento => mudarCor(evento.target.value, time.nome)} type="color" name="input-cor" value={time.cor} />
+            <input onChange={evento => mudarCor(evento.target.value, time.id)} type="color" name="input-cor" value={time.cor} />
             <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
             <div className='colaboradores'>
                 {colaboradores.map((colaborador, indice) => {
